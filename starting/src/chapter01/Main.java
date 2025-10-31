55560f8bdf62041883dd5e7d734da6cbccb2cd77
Meryem Akkamis <akkamismeryem@gmail.com>
2025-10-31 13:53:16 +0300

    Initial commit..

---
+++ starting/src/chapter01/Main.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class Main {
+        public static void main(String[] args){
+            System.out.println("Chapter 01");
+            System.out.println("Continue..");
+
+        }
+}
+
